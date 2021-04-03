50539ac413442e0962334a96786b571a1fca8008
Liam Donovan <grandlembadon@gmail.com>
2021-04-02 22:29:40 -0400

    Committing Practice work

--- src/Practice.java
+++ src/Practice.java
@@ -1,2 +1,9 @@
 public class Practice {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        }
+        return false;
+    }
+
 }
